c214d5e0b7da11a951477cf456e19e96a2bea0d7
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-19 19:08:49 -0300

    Add project files.

---
+++ AreaDoCirculo/Program.cs
@@ -0,0 +1,9 @@
+﻿double n, raio, area;
+
+Console.WriteLine("Digite o valor do raio para calcular a área do círculo.");
+n = 3.14159;
+raio = double.Parse(Console.ReadLine());
+area = n*(Math.Pow(raio, 2));
+
+Console.WriteLine("A="+area.ToString("F4"));
+
